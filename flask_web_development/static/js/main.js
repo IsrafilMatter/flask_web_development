9b95a386d3bd9de6a0d4192a6eb8761ad08834e4
IsrafilMatter <148543830+IsrafilMatter@users.noreply.github.com>
2025-10-08 22:50:01 +0800

    Add files via upload

---
+++ flask_web_development/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // simple focus on first input in forms
+  var firstInput = document.querySelector('main form input');
+  if (firstInput) firstInput.focus();
+});
+
